008c906cc67e789a865eab580be69f0b7963c23b
andryudianto <andriyudianto60@gmail.com>
2020-08-27 20:56:05 +0700

    challange

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,14 @@
 const express = require(`express`)
 const Router = express.Router()
 
+const ControllerUser = require('../controllers/controllerUser.js')
 
-
+Router.get('/register', ControllerUser.getRegisterHandler)
+Router.post('/register', ControllerUser.postRegisterHandler)
+Router.get('/edit/:id', ControllerUser.getEditProfileHandler)
+Router.post('/edit:id', ControllerUser.postEditProfileHandler)
+Router.get('/login', ControllerUser.getLoginHandler)
+Router.post('/login', ControllerUser.postLoginHandler)
+Router.post('/logout', ControllerUser.logoutHandler)
 
 module.exports = Router
